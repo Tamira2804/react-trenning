f147473fb10e2e774e7b4f029def80163a907dec
Tamira2804 <tamira2804@gmail.com>
2023-08-08 19:23:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from 'App';
+import ReactDOM from 'react-dom';
+import App from './App';
 import 'modern-normalize/modern-normalize.css';
-import './styles.css';
+import './styles/base.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
