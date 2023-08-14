f5c358a27cb32ab89fcd7f9b8ae7782caf9e716a
Tamira2804 <tamira2804@gmail.com>
2023-08-14 12:01:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'modern-normalize/modern-normalize.css';
 import './styles/base.scss';
 
-ReactDOM.render(
+// Використовуємо createRoot замість ReactDOM.render
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
