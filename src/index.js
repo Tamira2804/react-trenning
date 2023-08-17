41dc48f7adad627c0563595242ddc1993f784bc9
Tamira2804 <tamira2804@gmail.com>
2023-08-17 13:28:18 +0300

    Player

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
-// import { Example1 } from 'components/Example1';
+import { Example1 } from './Example1';
 import { Reader } from 'components/Reader/Reader';
 import './index.css';
 import publications from './publications.json';
@@ -13,6 +13,7 @@ createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Example1 />
       <Reader items={publications} />
     </ThemeProvider>
   </React.StrictMode>
