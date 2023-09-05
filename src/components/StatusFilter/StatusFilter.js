4460a8f6f4ccbbaffdcde1f6e9fc338020a8cc15
Tamira2804 <tamira2804@gmail.com>
2023-09-05 09:38:10 +0300

    added store & actions

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,37 @@
+import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'components/Button/Button';
+import { statusFilters } from '../../redux/constans';
+import { getStatusFilter } from '../../redux/selectors';
+import { setStatusFilter } from '../../redux/actions';
+
 import css from './StatusFilter.module.css';
 
 export const StatusFilter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getStatusFilter);
+
+  const hadleFilterChange = filter => dispatch(setStatusFilter(filter));
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => hadleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => hadleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.complited}
+        onClick={() => hadleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
